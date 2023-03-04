0051f5fc596b86aa4ccc4f11c04a9d6dcbd2ca10
Reynald <reynaldquiniloh5@gmail.com>
2023-03-04 21:01:16 +0800

    add exam no.1

--- prelim/problem-sets/seatworks/exams/exam_1/src/index.ts
+++ prelim/problem-sets/seatworks/exams/exam_1/src/index.ts
@@ -1,40 +1,26 @@
 function prompt(question: string, callback: Function): void {
-    const stdin = process.stdin,
-      stdout = process.stdout
-  
-    stdin.resume()
-    stdout.write(question)
-  
-    stdin.once('data', function (data): void {
-      callback(data.toString().trim())
+  const stdin = process.stdin,
+    stdout = process.stdout
+
+  stdin.resume()
+  stdout.write(question)
+
+  stdin.once('data', function (data): void {
+    callback(data.toString().trim())
+  })
+}
+
+prompt('Enter Height:', function (inputHeight: string): void {
+  const Height = +inputHeight
+  if (Height <= 0) console.log(`invalid`)
+
+  else{
+    prompt('Enter Base:', function(inputBase: string): void{
+      const Base = +inputBase
+      const Hypotenuse= Math.pow(Height,2)+Math.pow(Base,2)
+      const Result= Math.sqrt(Hypotenuse)
+      console.log(`The Result:${Result}`)
+
     })
   }
-  
-prompt('Input the A value:', function (InputHeigth: string ): void {
-    prompt('Input the B value:', function (InputBase: string ): void {
-        prompt('Input the C value:', function (InputHypotenuse: string ): void{
-
-
-            let A = +InputHeigth
-            let B = +InputBase
-            let C = +InputHypotenuse
-
-
-            const RightAngle = (`C + math.s`)
-
-
-            console.log(`The calculation of the Hypotenuse of a RightAngle in meter is: ${RightAngle}`)
-
-
-
-        })
-
-    })
-
-
 })
-    
-
-
-
-

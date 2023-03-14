8d23cdbd6bcd9c216cb09e49a5a2ce833e9f73fd
Reynald <reynaldquiniloh5@gmail.com>
2023-03-14 22:38:36 +0800

    add Multiplication Table

--- Self Learning/Multiplication Table/src/index.ts
+++ Self Learning/Multiplication Table/src/index.ts
@@ -1,9 +1,14 @@
+function prompt(question: string, callback: Function): void {
+  const stdin = process.stdin,
+    stdout = process.stdout
 
+  stdin.resume()
+  stdout.write(question)
 
-
-
-
-
+  stdin.once('data', function (data): void {
+    callback(data.toString().trim())
+  })
+}
 
 function createMultiplicationTable(rows: number, cols: number): number[][] {
     const table: number[][] = [];

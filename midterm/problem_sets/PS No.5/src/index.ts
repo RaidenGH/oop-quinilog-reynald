a1bbfa1efb964ac15e81100b66d846f951dddc5f
Reynald <reynaldquiniloh5@gmail.com>
2023-03-28 00:45:07 +0800

    add ps no.5

--- midterm/problem_sets/PS No.5/src/index.ts
+++ midterm/problem_sets/PS No.5/src/index.ts
@@ -8,34 +8,34 @@
 // function input => [55, 1, 3, 2, 10]
 // output => [1, 2, 3, 10, 55]
 
-
 function prompt(question: string, callback: Function): void {
   const stdin = process.stdin,
     stdout = process.stdout
 
-  stdin.resume()
-  stdout.write(question)
-
-  stdin.once('data', function (data): void {
-    callback(data.toString().trim())
-  })
-}
-
-function sortNumbers(numbers: number[]): number[] {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] > numbers[j]) {
-        
-        const temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
-      }
-    }
+    stdin.resume()
+    stdout.write(question)
+    
+    stdin.once('data', function (data): void {
+      callback(data.toString().trim())
+    })
   }
-  return numbers;
-}
 
-
-const input = [55, 1, 3, 2, 10];
-const sortedOutput = sortNumbers(input);
-console.log(sortedOutput); 
+  prompt('Enter Numbers: ', function (inputNumbers: string): void {
+    const numbers = inputNumbers.split(',').map(Number)
+    
+    function sortArray(numbers: number[]): number[]{
+      for (let i = 0; i < numbers.length - 1; i++){
+        for (let j = i + 1; j < numbers.length; j++){
+          if (numbers[j] < numbers[i]){
+            const array = numbers[j]
+            numbers[j] = numbers[i]
+            numbers[i] = array
+          }
+        }
+      }
+      return numbers
+    }
+    const sortedNumbers = sortArray(numbers)
+    console.log('Sorted numbers: ', sortedNumbers)
+  })
+    

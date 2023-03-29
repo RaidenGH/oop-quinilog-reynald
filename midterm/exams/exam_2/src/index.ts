7ec27f854c5eeb4705db78a369884694c295c0bf
Reynald <reynaldquiniloh5@gmail.com>
2023-03-29 19:42:43 +0800

    add exam no.2

--- midterm/exams/exam_2/src/index.ts
+++ midterm/exams/exam_2/src/index.ts
@@ -1,3 +1,18 @@
+// Exam No. 2
+
+// Create a function that will accept an array of numbers (in any order) and sort it from largest to smallest.
+
+// It should be built using loops.
+
+// Example:
+
+// function input => [55, 1, 3, 2, 10]
+// output => [55, 10, 3, 2, 1]
+// Another example:
+
+// function input => [10, -30, 0, -2, 3]
+// output => [10, 3, 0, -2, -30]
+
 function prompt(question: string, callback: Function): void {
   const stdin = process.stdin,
     stdout = process.stdout
@@ -10,3 +25,23 @@ function prompt(question: string, callback: Function): void {
   })
 }
 
+function sortingArray(array : number[]): number []{
+  let num = 0
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      if (array[j] > array[i]) {
+        num = array[j]
+        array[j] = array[i]
+        array[i] = num
+      }
+    }
+  }
+  return array
+}
+
+prompt('Enter Function Input:',function(inputFunction:string):void{
+  const Function = inputFunction.split(',').map(Number)
+
+  const sortedArray= sortingArray(Function)
+  console.log(`Sorted Array:`,sortedArray)
+})

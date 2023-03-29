9707fa5566d7c6d5cc2653441abfe75689d8ae97
Reynald <reynaldquiniloh5@gmail.com>
2023-03-29 19:19:23 +0800

    add exam no.1

---
+++ midterm/exams/exam_1/src/index.ts
@@ -0,0 +1,41 @@
+// Exam No. 1
+
+// Create a program that will accept a positive integer and print the divisible by 5 numbers starting from 0
+
+// Example:
+
+// Enter a positive integer: 26
+
+// Output:
+// 5
+// 10
+// 15
+// 20
+// 25
+// Another example:
+
+// Enter a positive integer: 10
+
+// Output:
+// 5
+// 10
+
+function prompt(question: string, callback: Function): void {
+  const stdin = process.stdin,
+    stdout = process.stdout
+
+  stdin.resume()
+  stdout.write(question)
+
+  stdin.once('data', function (data): void {
+    callback(data.toString().trim())
+  })
+}
+prompt('Enter a Positive Enteger:',function(inputInteger:number):void{
+  console.log('Output:')
+  for(let i=1; i <= inputInteger;i++){
+    if (i % 5===0){
+      console.log(i)
+    }
+  }
+})
---
+++ midterm/exams/exam_3/src/index.ts
@@ -0,0 +1,12 @@
+function prompt(question: string, callback: Function): void {
+  const stdin = process.stdin,
+    stdout = process.stdout
+
+  stdin.resume()
+  stdout.write(question)
+
+  stdin.once('data', function (data): void {
+    callback(data.toString().trim())
+  })
+}
+

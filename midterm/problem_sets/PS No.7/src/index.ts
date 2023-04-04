59ed1739531d83af35160ff166ce92e011901cac
Reynald <reynaldquiniloh5@gmail.com>
2023-04-04 17:56:30 +0800

    add ps no.7

--- midterm/problem_sets/PS No.7/src/index.ts
+++ midterm/problem_sets/PS No.7/src/index.ts
@@ -7,71 +7,55 @@
 
 function prompt(question: string, callback: Function): void {
   const stdin = process.stdin,
-    stdout = process.stdout;
+    stdout = process.stdout
 
-  stdin.resume();
-  stdout.write(question);
+  stdin.resume()
+  stdout.write(question)
 
   stdin.once('data', function (data): void {
-    callback(data.toString().trim());
-  });
-}
-
-function calculateSum(numbers: number[]): number {
-  let total = 0;
-  for (let number of numbers) {
-    total += number;
-  }
-  return total;
-}
-
-function calculateMean(numbers: number[]): number {
-  if (numbers.length === 0) {
-    return 0;
-  } else {
-    return calculateSum(numbers) / numbers.length;
-  }
-}
-
-function calculateMedian(numbers: number[]): number {
-  const sortedNumbers = numbers.slice().sort((a, b) => a - b);
-  const length = sortedNumbers.length;
-  const middle = Math.floor(length / 2);
-  if (length % 2 === 0) {
-    return (sortedNumbers[middle-1] + sortedNumbers[middle]) / 2;
-  } else {
-    return sortedNumbers[middle];
-  }
-}
-
-function calculateMode(numbers: number[]): number {
-  let modeCount = 0;
-  let mode = 0;
-  for (let number of numbers) {
-    const count = numbers.filter(n => n === number).length;
-    if (count > modeCount) {
-      modeCount = count;
-      mode = number;
-    }
-  }
-  return mode;
+    callback(data.toString().trim())
+  })
 }
 
 function calculatePower(base: number, exponent: number): number {
-  let result = 1;
+  let result = 1
+
   for (let i = 0; i < exponent; i++) {
-    result *= base;
+    result *= base
   }
-  return result;
+
+  return result
 }
 
-prompt('Enter The Numbers:',function(inputNumbers:string):void{
-  const numbers = inputNumbers.split(',').map(Number);
-  
-  console.log(`Numbers: ${numbers}`);
-  console.log('Sum:', calculateSum(numbers));
-  console.log('Mean:', calculateMean(numbers));
-  console.log('Median:', calculateMedian(numbers));
-  console.log('Mode:', calculateMode(numbers));
-  console.log('Power:', calculatePower(2, 3));  
-});
+prompt('Enter First Input:', function(inputFirstNumber: string): void{
+  prompt('Select "+", "-", "*", "/", "^" for operator:', function(inputOperator: string):void{
+    prompt('Enter second input:', function(inputSecondNumber: string):void {
+      let FirstNumber = +inputFirstNumber
+      let SecondNumber = +inputSecondNumber
+      let result;
+
+      for (let i = 0; i < 1; i++) {
+        if (inputOperator === '+') {
+          result = FirstNumber + SecondNumber
+          console.log(`${FirstNumber} + ${SecondNumber} = ${result}`)
+        } else if (inputOperator === '-') {
+          result = FirstNumber - SecondNumber
+          console.log(`${FirstNumber} - ${SecondNumber} = ${result}`)
+        } else if (inputOperator === '*') {
+          result = FirstNumber * SecondNumber
+          console.log(`${FirstNumber} * ${SecondNumber} = ${result}`)
+        } else if (inputOperator === '/') {
+          result = FirstNumber / SecondNumber
+          console.log(`${FirstNumber} / ${SecondNumber} = ${result}`)
+        } else if (inputOperator === '^') {
+          result = calculatePower(FirstNumber, SecondNumber)
+          console.log(`${FirstNumber} ^ ${SecondNumber} = ${result}`)
+        } else {
+          console.log('Invalid Operator')
+        }
+      }
+    })
+  })
+})
+
+

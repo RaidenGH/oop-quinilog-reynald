a3b1d8fc812b3421ce38f05869b1abd6892e2426
Reynald B. Quinilog <122993993+RaidenGH@users.noreply.github.com>
2023-05-15 07:35:03 -0400

    add ps no. 10

--- semifinal/Problem-Sets/ps-no-10/src/index.ts
+++ semifinal/Problem-Sets/ps-no-10/src/index.ts
@@ -1,12 +1,41 @@
-function prompt(question: string, callback: Function): void {
-  const stdin = process.stdin,
-    stdout = process.stdout
+// Problem Set No. 10
 
-  stdin.resume()
-  stdout.write(question)
+// Create a Circle class that accepts a Radius, Height initialization and implements the following methods:
 
-  stdin.once('data', function (data): void {
-    callback(data.toString().trim())
-  })
+// diameter
+
+// circumference
+
+// area
+
+// volume
+class Circle {
+  private radius: number
+  private height: number
+
+  constructor(radius: number, height: number) {
+    this.radius = radius
+    this.height = height
+  }
+
+  getDiameter(): number {
+    return 2 * this.radius
+  }
+
+  getCircumference(): number {
+    return 2 * Math.PI * this.radius
+  }
+
+  getArea(): number {
+    return Math.PI * this.radius * this.radius
+  }
+
+  getVolume(): number {
+    return Math.PI * this.radius * this.radius * this.height
+  }
 }
-
+const circle = new Circle(10, 23)
+console.log(circle.getDiameter())
+console.log(circle.getCircumference())
+console.log(circle.getArea())
+console.log(circle.getVolume())
